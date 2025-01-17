2688180d7213e18af3b5cf02cce616d169b93bd8
brandsgroup <vikasinfraconstructions>
2025-01-17 16:28:11 +0530

    once again try

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+#!/bin/bash
+# build.sh
 
 # Install dependencies
 echo "Installing dependencies..."
-pip install --upgrade pip
-pip install -r requirements.txt
-
+# pip install --upgrade pip
+# pip install -r requirements.txt
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install -r requirements.txt
 # Apply migrations
 echo "Running migrations..."
 python manage.py makemigrations --noinput
